5eab68e6f18141718d5991ebdd375abf85be9d96
trener <trener@00VM>
2024-06-26 10:44:09 +0000

    Add project files.

---
+++ HelloWorld/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.listCity = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.checkBoxC = new System.Windows.Forms.CheckBox();
+            this.checkBoxPython = new System.Windows.Forms.CheckBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.pictureBoxObraz = new System.Windows.Forms.PictureBox();
+            this.labelError = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxObraz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Witaj aplikacjo!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.labelError);
+            this.groupBox1.Controls.Add(this.pictureBoxObraz);
+            this.groupBox1.Controls.Add(this.buttonSubmit);
+            this.groupBox1.Controls.Add(this.checkBoxPython);
+            this.groupBox1.Controls.Add(this.checkBoxC);
+            this.groupBox1.Controls.Add(this.radioFemale);
+            this.groupBox1.Controls.Add(this.radioMale);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.listCity);
+            this.groupBox1.Controls.Add(this.textAddress);
+            this.groupBox1.Controls.Add(this.textName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(25, 65);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(884, 521);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Address";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(107, 52);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(276, 26);
+            this.textName.TabIndex = 2;
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(107, 99);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(276, 26);
+            this.textAddress.TabIndex = 3;
+            this.textAddress.TextChanged += new System.EventHandler(this.textAddress_TextChanged);
+            // 
+            // listCity
+            // 
+            this.listCity.FormattingEnabled = true;
+            this.listCity.ItemHeight = 20;
+            this.listCity.Items.AddRange(new object[] {
+            "Kielce",
+            "Radom",
+            "Warszawa",
+            "Kraków"});
+            this.listCity.Location = new System.Drawing.Point(107, 152);
+            this.listCity.Name = "listCity";
+            this.listCity.Size = new System.Drawing.Size(276, 104);
+            this.listCity.TabIndex = 2;
+            this.listCity.SelectedIndexChanged += new System.EventHandler(this.listCity_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Miasta";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(107, 281);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(106, 24);
+            this.radioMale.TabIndex = 4;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Mezczyzna";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(107, 326);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(81, 24);
+            this.radioFemale.TabIndex = 5;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Kobieta";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxC
+            // 
+            this.checkBoxC.AutoSize = true;
+            this.checkBoxC.Location = new System.Drawing.Point(107, 373);
+            this.checkBoxC.Name = "checkBoxC";
+            this.checkBoxC.Size = new System.Drawing.Size(48, 24);
+            this.checkBoxC.TabIndex = 2;
+            this.checkBoxC.Text = "C#";
+            this.checkBoxC.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPython
+            // 
+            this.checkBoxPython.AutoSize = true;
+            this.checkBoxPython.Location = new System.Drawing.Point(107, 413);
+            this.checkBoxPython.Name = "checkBoxPython";
+            this.checkBoxPython.Size = new System.Drawing.Size(77, 24);
+            this.checkBoxPython.TabIndex = 6;
+            this.checkBoxPython.Text = "Python";
+            this.checkBoxPython.UseVisualStyleBackColor = true;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.buttonSubmit.Location = new System.Drawing.Point(146, 467);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(169, 34);
+            this.buttonSubmit.TabIndex = 7;
+            this.buttonSubmit.Text = "SUBMIT";
+            this.buttonSubmit.UseVisualStyleBackColor = false;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // pictureBoxObraz
+            // 
+            this.pictureBoxObraz.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxObraz.Image")));
+            this.pictureBoxObraz.Location = new System.Drawing.Point(684, 52);
+            this.pictureBoxObraz.Name = "pictureBoxObraz";
+            this.pictureBoxObraz.Size = new System.Drawing.Size(178, 176);
+            this.pictureBoxObraz.TabIndex = 8;
+            this.pictureBoxObraz.TabStop = false;
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(680, 252);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 20);
+            this.labelError.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 621);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxObraz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.ListBox listCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.CheckBox checkBoxPython;
+        private System.Windows.Forms.CheckBox checkBoxC;
+        private System.Windows.Forms.PictureBox pictureBoxObraz;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ HelloWorld/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string text = listCity.GetItemText(listCity.SelectedItem);
+            MessageBox.Show(text);
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            string name = textName.Text;
+            string address = textAddress.Text;
+            if (ValidateAddress(address))
+            {
+                labelError.Text = "Adres jest nieprawidlowy";
+                labelError.ForeColor = System.Drawing.Color.Red;
+            }
+
+            MessageBox.Show(name + " " + address);
+        }
+
+        private void textAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool ValidateAddress(string address)
+        {
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                return true;
+            }
+            string pattern = @"^(?=.*[a-zA-Z])(?=.*\d).+$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(address);
+        }
+    }
+}
